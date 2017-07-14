33075f6444279848b4d9399dc708ec77ffd09193
Joshua Pack <joshua@joshuapack.com>
2017-07-14 16:14:48 -0400

    initial commit for code

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () =>
+<div style={{ color: `tomato` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="http://lorempixel.com/400/200/" />
+    <br />
+    <Link to="/my-second-gatsby-page/">Link</Link>
+    <Link to="/counter/">Counter</Link>
+</div>
